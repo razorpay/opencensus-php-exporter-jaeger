25d005adffada2e5d85e3167d6213c76ed87995a
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-05-12 20:10:32 +0530

    [config] trace config file

---
+++ config/trace.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    /*
+     * The following debug options come
+     * into play only 'debug' is true.
+     *
+     * They define the places where the
+     * logs will be written.
+     */
+    'debug_options' => [
+        'screen' => false,
+        'browser' => false,
+        'chrome' => false
+    ],
+
+    'channel' => 'AUTH-SERVICE',
+
+    'cacheDir' => storage_path('framework/cache/'),
+
+    /*
+     * Displays line/file/class/method from which the log call originated
+     */
+    'introspection' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Path for trace logs
+    |--------------------------------------------------------------------------
+    */
+    'logpath' => storage_path(). '/logs/trace.log',
+
+    'mockAws' => false,
+
+    'fallbackEmail' => 'developers@razorpay.com'
+];
