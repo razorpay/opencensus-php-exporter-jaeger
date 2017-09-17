c8e3db37804b9919922e2d6459548c0ee92b01fb
Venkatesan <venkat.v@razorpay.com>
2017-09-17 17:26:44 +0530

    - adds kubernetes related changes: adds related manifests - adds support for commit.txt via adding the commit hash to the commit.txt and serving via nginx - fixes local docker based development - adds ignore for kubernetes secrets from git - minor modifications in docker boot script

--- dockerconf/boot.sh
+++ dockerconf/boot.sh
@@ -4,22 +4,25 @@ mkdir -p /var/log/nginx
 touch /var/log/nginx/auth.access.log
 touch /var/log/nginx/auth.error.log
 
+echo $GIT_COMMIT_HASH > /app/commit.txt
+
 ALOHOMORA_BIN=$(which alohomora)
 
 # Lumen in docker picks system environment variable from .env
 echo "APP_MODE=${APP_MODE}" > /app/.env
 
+$ALOHOMORA_BIN cast --region ap-south-1 --env $APP_MODE --app auth "dockerconf/auth.nginx.conf.j2"
+cp dockerconf/auth.nginx.conf /etc/nginx/conf.d/auth.conf
+
 if [[ "${APP_MODE}" == "dev" ]]; then
-  cp dockerconf/auth.dev.nginx.conf /etc/nginx/conf.d/auth.conf && \
   cp environment/.env.docker environment/.env.testing && \
   cp environment/env.sample.php environment/env.php && \
-  sed -i 's/dev/testing/g' ./environment/env.php
+  sed -i 's/dev/testing/g' ./environment/env.php && \
+  sed -i "s|dev-auth.razorpay.com|auth.razorpay.dev|g" /etc/nginx/conf.d/auth.conf
 else
     # casting alohomora to unlock the secrets
   $ALOHOMORA_BIN cast --region ap-south-1 --env $APP_MODE --app auth "environment/.env.vault.j2"
   $ALOHOMORA_BIN cast --region ap-south-1 --env $APP_MODE --app auth "environment/env.php.j2"
-  $ALOHOMORA_BIN cast --region ap-south-1 --env $APP_MODE --app auth "dockerconf/auth.nginx.conf.j2"
-  cp dockerconf/auth.nginx.conf /etc/nginx/conf.d/auth.conf
 fi
 
 # Fix permissions
