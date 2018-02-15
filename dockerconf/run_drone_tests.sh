a1bc50dc8e1a64b7ffeee4a5c672fa6a4cccc899
Tasdik Rahman <prodicus@outlook.com>
2018-02-15 23:25:00 +0530

    drone: init

---
+++ dockerconf/run_drone_tests.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -euo pipefail
+
+SRC_DIR=/drone/src/github.com/razorpay/auth
+auth_TMP_DIR=/tmp/auth ## defined in the environment file
+
+function init_setup
+{
+    apk update
+    echo "changing dir to $SRC_DIR"
+    cd $SRC_DIR
+
+    echo "copying env file for testing"
+    cp ./environment/.env.sample ./environment/.env.testing
+
+    echo "running composer install"
+    composer config -g github-oauth.github.com ${GIT_TOKEN}
+    composer install --no-interaction
+
+    if [ ! -d "$auth_TMP_DIR" ]; then
+        mkdir -p $auth_TMP_DIR
+    fi
+}
+
+function run_tests
+{
+    cd $SRC_DIR
+    export APP_MODE=testing
+    echo "APP_MODE=testing" > .env
+
+    # Run Migrations
+    echo "running migrations"
+    APP_ENV=testing php artisan migrate
+
+    # Run tests
+    echo "running tests"
+    APP_MODE=testing php -d memory_limit=1024M vendor/phpunit/phpunit/phpunit --debug --verbose
+}
+
+init_setup
+run_tests
+exit $?
