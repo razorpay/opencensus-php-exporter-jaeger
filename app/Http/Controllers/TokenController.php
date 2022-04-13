0db93dc95c88603fac5ec2cd053a15d1eb4372ca
Arpit Verma <arpit.verma@razorpay.com>
2022-04-13 12:25:04 +0530

    testing on devstack

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -85,7 +85,7 @@ class TokenController extends Controller
 
         $token = (new Token\Repository)->findOrFailPublic($id);
 
-        $this->service->revoketken($id, $input);
+        $this->service->revokeToken($id, $input);
 
         $this->revokeMerchantApplicationMapping($token, $input);
 
