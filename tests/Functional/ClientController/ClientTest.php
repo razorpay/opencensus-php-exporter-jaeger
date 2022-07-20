82f4846b9d02942181b140859805b6cc024d7812
Anmol <105702910+AnmolSidhu16@users.noreply.github.com>
2022-07-20 19:14:59 +0530

    Functional Tests - Auth Service: POST/DELETE client (#271)
    * Functional Tests - Auth Service - 49 to 90%: adding POST/DELETE client Functional Test cases
    
    * Functioinal Tests - Auth Service - 0 to 90%: create/delete clients review comments

--- tests/Functional/ClientController/ClientTest.php
+++ tests/Functional/ClientController/ClientTest.php
@@ -2,10 +2,10 @@
 
 namespace App\Tests\Functional\ClientController;
 
+use App\Tests\Concerns\RequestResponseFlowTrait;
+use App\Tests\TestCase as TestCase;
 use Razorpay\OAuth\Application;
 use Razorpay\OAuth\Client;
-use App\Tests\TestCase as TestCase;
-use App\Tests\Concerns\RequestResponseFlowTrait;
 
 class ClientTest extends TestCase
 {
@@ -45,6 +45,84 @@ class ClientTest extends TestCase
         $this->assertNotEquals($this->prodClient["id"],$newClients["prod"]["id"]);
     }
 
+    public function testCreateClients()
+    {
+        $this->createTestApp();
+
+        $data = &$this->testData[__FUNCTION__];
+
+        $data['request']['url'] = '/clients';
+
+        $data['request']['content']['application_id'] = $this->application->getId();
+
+        $response = $this->runRequestResponseFlow($data);
+
+        $newClients = $response['client_details'];
+
+        $this->assertValidClient($newClients['dev']);
+
+        $this->assertValidClient($newClients['prod']);
+    }
+
+    public function testCreateClientsWithNoMerchant()
+    {
+        $this->createTestApp();
+
+        $data = &$this->testData[__FUNCTION__];
+
+        $data['request']['url'] = '/clients';
+
+        $data['request']['content']['application_id'] = $this->application->getId();
+
+        $this->startTest($data);
+    }
+
+    public function testCreateClientsWithWrongMerchantId()
+    {
+        $this->createTestApp();
+
+        $data = &$this->testData[__FUNCTION__];
+
+        $data['request']['url'] = '/clients';
+
+        $data['request']['content']['application_id'] = $this->application->getId();
+
+        $this->startTest($data);
+    }
+
+    public function testDeleteClients()
+    {
+        $this->createTestApp();
+
+        $data = &$this->testData[__FUNCTION__];
+
+        $data['request']['url'] = '/clients/'.$this->devClient->id;
+
+        $response = $this->runRequestResponseFlow($data);
+
+        $this->assertEquals([],$response);
+    }
+
+    public function testDeleteNoRecordClients()
+    {
+        $data = &$this->testData[__FUNCTION__];
+
+        $data['request']['url'] = '/clients/dummy_client';
+
+        $this->runRequestResponseFlow($data);
+    }
+
+    public function testDeleteClientsWithNoMerchant()
+    {
+        $this->createTestApp();
+
+        $data = &$this->testData[__FUNCTION__];
+
+        $data['request']['url'] = '/clients/dummy_client';
+
+        $this->startTest($data);
+    }
+
     public function testRefreshClientsInvalidInput()
     {
         $this->createTestApp();
@@ -68,6 +146,17 @@ class ClientTest extends TestCase
         $this->prodClient = factory(Client\Entity::class)->create(['application_id' => $this->application->getId(), 'environment' => 'prod']);
 
         $this->devClient = factory(Client\Entity::class)->create(['application_id' => $this->application->getId(), 'environment' => 'dev']);
+    }
+
+    protected function assertValidClient(array $content)
+    {
+        $this->assertArrayHasKey('id', $content);
+        $this->assertArrayHasKey('merchant_id', $content);
+        $this->assertArrayHasKey('secret', $content);
+        $this->assertArrayHasKey('type', $content);
+        $this->assertArrayHasKey('application_id', $content);
+        $this->assertArrayHasKey('environment', $content);
+        $this->assertArrayHasKey('type', $content);
     }
 
 }
--- tests/Functional/ClientController/ClientTestData.php
+++ tests/Functional/ClientController/ClientTestData.php
@@ -2,6 +2,116 @@
 
 return [
 
+    'testCreateClients' => [
+        'request'  => [
+            'method'  => 'POST',
+            'content' => [
+                'merchant_id' => '10000000000000',
+            ]
+        ],
+        'response' => [
+            'content' => [
+                'merchant_id' => '10000000000000',
+                'name'        => 'apptest',
+                'website'     => 'https://www.example.com',
+                'type'        => 'partner'
+            ]
+        ]
+    ],
+
+    'testCreateClientsWithNoMerchant' => [
+        'request'  => [
+            'method'  => 'POST',
+            'content' => []
+        ],
+        'response' => [
+            'content' => [
+                'error' => [
+                    'description' => 'Validation failed. The merchant id field is required.',
+                ],
+            ],
+            'status_code' => 500,
+        ],
+        'exception' => [
+            'class'   => 'Razorpay\Spine\Exception\ValidationFailureException',
+            'message' => 'Validation failed. The merchant id field is required.',
+        ]
+    ],
+
+    'testCreateClientsWithWrongMerchantId' => [
+        'request'  => [
+            'method'  => 'POST',
+            'content' => [
+                'merchant_id' => '1000',
+            ]
+        ],
+        'response' => [
+            'content' => [
+                'error' => [
+                    'description' => 'Validation failed. The merchant id must be 14 characters.',
+                ],
+            ],
+            'status_code' => 500,
+        ],
+        'exception' => [
+            'class'   => 'Razorpay\Spine\Exception\ValidationFailureException',
+            'message' => 'Validation failed. The merchant id must be 14 characters.',
+        ]
+    ],
+
+    'testDeleteClients' => [
+        'request'  => [
+            'method'  => 'DELETE',
+            'content' => [
+                'merchant_id' => '10000000000000',
+            ]
+        ],
+        'response' => [
+            'content' => []
+        ]
+    ],
+
+    'testDeleteNoRecordClients' => [
+        'request'  => [
+            'method'  => 'DELETE',
+            'content' => [
+                'merchant_id' => '10000000000000',
+            ]
+        ],
+        'response' => [
+            'content' => [
+                'error' => [
+                    'description' => 'No records found with the given Id',
+                ],
+            ],
+            'status_code' => 400,
+        ],
+        'exception' => [
+            'class'   => 'Razorpay\OAuth\Exception\DBQueryException',
+            'message' => 'No records found with the given Id',
+        ]
+    ],
+
+    'testDeleteClientsWithNoMerchant' => [
+        'request'  => [
+            'method'  => 'DELETE',
+            'content' => [
+            ]
+        ],
+        'response' => [
+            'content' => [
+                'error' => [
+                    'description' => 'Validation failed. The merchant id field is required.',
+                ],
+            ],
+            'status_code' => 500,
+        ],
+        'exception' => [
+            'class'   => 'Razorpay\Spine\Exception\ValidationFailureException',
+            'message' => 'Validation failed. The merchant id field is required.',
+        ]
+    ],
+
     'testRefreshClients' => [
         'request'  => [
             'method'  => 'PUT',
